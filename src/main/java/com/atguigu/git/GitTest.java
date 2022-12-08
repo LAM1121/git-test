0b05e2f8853a9a4de47220605c1163c3f3e2e0cf
LAM <1422755682@qq.com>
2022-12-08 16:06:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Package: com.atguigu.git
+ * @ClassName: GitTest
+ * @Author: LAM
+ * @CreateTime: 2022/12/8 16:02
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!!!");
+    }
+}
